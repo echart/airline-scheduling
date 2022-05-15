19e2bfd2097a5b5aa06f22e6839987a89e93dc8e
˜Willians <willians.echart@possible.com>
2022-05-15 14:02:29 -0300

    chore: adding messages file

---
+++ src/config/messages.js
@@ -0,0 +1,10 @@
+const messages = {
+  rotation: {
+    add: 'Add flight to rotation',
+    remove: 'Remove flight from rotation',
+    empty: 'There are no flights for the selected aircraft.',
+    confirm_remove: 'This action will delete this flight and all subsequent ones, are you sure?'
+  }
+}
+
+export default messages;
