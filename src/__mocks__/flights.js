dbc614117663639cec01c09df8053a05e13f74cc
˜Willians <willians.echart@possible.com>
2022-05-15 16:23:07 -0300

    mock: add mock for flights

---
+++ src/__mocks__/flights.js
@@ -0,0 +1,31 @@
+const flights = [
+  {
+    id: 'AS1002',
+    departuretime: 27900,
+    arrivaltime: 32100,
+    readable_departure: '07:45',
+    readable_arrival: '08:55',
+    origin: 'LFMN',
+    destination: 'LFSB',
+  },
+  {
+    id: 'AS1025',
+    departuretime: 22800,
+    arrivaltime: 28200,
+    readable_departure: '06:20',
+    readable_arrival: '07:50',
+    origin: 'LFSB',
+    destination: 'EDDH',
+  },
+  {
+    id: 'AS1026',
+    departuretime: 30000,
+    arrivaltime: 35100,
+    readable_departure: '08:20',
+    readable_arrival: '09:45',
+    origin: 'EDDH',
+    destination: 'LFSB',
+  },
+];
+
+export default flights;
